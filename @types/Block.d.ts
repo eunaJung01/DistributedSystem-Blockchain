c02af209aa0256a7cba9d329beee33d603a6c673
euna <christinejung10@daum.net>
2023-05-09 00:37:54 +0900

    chore: Block.d.ts 필드 추가 및 상속 수정

--- @types/Block.d.ts
+++ @types/Block.d.ts
@@ -1,13 +1,14 @@
-export interface IBlock {
-    merkleRoot: string;
-    hash: string;
-    data: string[];
-    height: number;
-}
-
 export interface IBlockHeader {
     version: string;
     height: number;
     timestamp: number;
     previousHash: string;
+}
+
+export interface IBlock extends IBlockHeader {
+    merkleRoot: string;
+    hash: string;
+    nonce: number;
+    height: number;
+    data: string[];
 }
