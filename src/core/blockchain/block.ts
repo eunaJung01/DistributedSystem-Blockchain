2a99eb20ae9f41b11254ff4dcbd512484d9c7e25
euna <christinejung10@daum.net>
2023-05-13 15:48:57 +0900

    fix: Block 생성자 속 필드 초기화 수정

--- src/core/blockchain/block.ts
+++ src/core/blockchain/block.ts
@@ -12,22 +12,19 @@ import {IBlock} from "Block";
 import {Failable} from "Failable";
 
 export class Block extends BlockHeader implements IBlock {
-    public hash: string;
-    public merkleRoot: string;
-    public nonce: number;
-    public difficulty: number;
-    public data: string[];
+    hash: string;
+    merkleRoot: string;
+    difficulty: number;
+    nonce: number;
+    data: string[];
 
     constructor(_previousBlock: Block, _data: string[], _adjustmentBlock: Block) {
-        const merkleRoot = Block.getMerkleRoot(_data);
-        const _header = new BlockHeader(_previousBlock);
-
         super(_previousBlock); // 상속받은 클래스의 속성을 모두 가져온다.
         this.hash = Block.createBlockHash(this);
-        this.merkleRoot = merkleRoot;
-        this.data = _data;
+        this.merkleRoot = Block.getMerkleRoot(_data);
+        this.difficulty = Block.getDifficulty(this, _adjustmentBlock, _previousBlock);
         this.nonce = 0;
-        this.difficulty = 0;
+        this.data = _data;
     }
 
     public static getGENESIS(): Block {
@@ -36,12 +33,20 @@ export class Block extends BlockHeader implements IBlock {
 
     public static getMerkleRoot<T>(_data: T[]): string {
         const merkleTree = merkle('sha256').sync(_data);
-        return merkleTree.root() || "0".repeat(64);
+        return merkleTree.root();
     }
 
     public static createBlockHash(_block: Block): string {
-        const {version, timestamp, merkleRoot, previousHash, height} = _block;
-        const values: string = `${version}${timestamp}${merkleRoot}${previousHash}${height}`;
+        const {
+            version,
+            timestamp,
+            height,
+            merkleRoot,
+            previousHash,
+            difficulty,
+            nonce
+        } = _block;
+        const values: string = `${version}${timestamp}${height}${merkleRoot}${previousHash}${difficulty}${nonce}`;
         return SHA256(values).toString();
     }
 
@@ -89,13 +94,13 @@ export class Block extends BlockHeader implements IBlock {
         const timeTaken: number = _newBlock.timestamp - _adjustmentBlock.timestamp;
         const timeExpected: number = BLOCK_GENERATION_TIME_UNIT * BLOCK_GENERATION_INTERVAL * DIFFICULTY_ADJUSTMENT_INTERVAL;
 
-        if (timeTaken == timeExpected) {
-            return _adjustmentBlock.difficulty;
-        }
         if (timeTaken < timeExpected / 2) {
             return _adjustmentBlock.difficulty + 1;
         }
-        return _adjustmentBlock.difficulty - 1;
+        if (timeTaken > timeExpected * 2) {
+            return _adjustmentBlock.difficulty - 1;
+        }
+        return _adjustmentBlock.difficulty;
     }
 
     static isValidNewBlock(_newBlock: Block, _previousBlock: Block): Failable<Block, string> {
