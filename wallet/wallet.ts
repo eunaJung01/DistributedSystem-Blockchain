5097a9dda886a6564189539ee09a7f42816d75b6
euna <christinejung10@daum.net>
2023-06-09 19:07:11 +0900

    feat: 지갑 클래스 구현

---
+++ wallet/wallet.ts
@@ -0,0 +1,48 @@
+import {randomBytes} from "crypto";
+import elliptic from "elliptic";
+import {SHA256} from "crypto-js";
+import fs from "fs";
+import path from "path";
+
+const dir = path.join(__dirname, "../data");
+
+const ec = new elliptic.ec("secp256k1");
+
+export class Wallet {
+    public account: string;
+    public privateKey: string;
+    public publicKey: string;
+    public balance: number;
+
+    // 1. privateKey를 인자값을 넣을 경우, 그 privateKey를 이용해 지갑 내용을 생성한다.
+    // 2. 인자값이 없을 경우, 기존에 만들어 놓은 메서드를 사용해 지갑 내용을 생성한다.
+    constructor(_privateKey: string = "") {
+        this.privateKey = _privateKey || this.getPrivateKey();
+        this.publicKey = this.getPublicKey();
+        this.account = this.getAccount();
+        this.balance = 0;
+
+        Wallet.createWallet(this);
+    }
+
+    // 파일 시스템(fs)를 통해 파일을 만들고 개인키를 저장한다.
+    static createWallet(myWallet: Wallet) {
+        const fileName = path.join(dir, myWallet.account);
+        const fileContent = myWallet.getPrivateKey();
+        fs.writeFileSync(fileName, fileContent);
+    }
+
+    public getAccount(): string {
+        return Buffer.from(this.publicKey).slice(26).toString();
+    }
+
+    public getPrivateKey(): string {
+        return randomBytes(32).toString("hex");
+    }
+
+    public getPublicKey(): string {
+        const keyPair = ec.keyFromPrivate(this.privateKey);
+        return keyPair.getPublic().encode("hex", true);
+    }
+
+}
