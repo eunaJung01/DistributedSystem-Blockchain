9c440f6560e2b24684dd8b0a7fd779dfb5823129
euna <christinejung10@daum.net>
2023-06-10 16:54:45 +0900

    fix: transaction 관련 인터페이스들 수정

--- @types/Transaction.d.ts
+++ @types/Transaction.d.ts
@@ -11,15 +11,15 @@ declare interface ITxIn {
 
 declare interface ITransaction {
     hash: string; // txIns, txOuts를 이용하여 만든 hash 값
-    txOuts: ITxOut;
-    txIns: ITxIn;
+    txOuts: ITxOut[];
+    txIns: ITxIn[];
 }
 
 // TxIn : UnspentTxOut[]울 참조해서 만들어진다.
 // TxIn을 만들 때는 UnspentTxOut[]애서 삭제한다.
 // TxOut을 만들 때는 UnspentTxOut[]에 생성한다.
 declare interface IUnspentTxOut {
-    txOutID: string; // TxOut을 담고 있는 트랜잭션의 hash 값
+    txOutId: string; // TxOut을 담고 있는 트랜잭션의 hash 값
     txOutIndex: number; // 트랜잭션의 txOuts 배열에서의 index 값
     account: string;
     amount: number;
